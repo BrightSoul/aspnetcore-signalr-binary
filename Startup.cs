37741c0cd64b32e95712fc7ed991aa5bcc622c41
Moreno Gentili <brightsoul@tiscali.it>
2020-11-14 18:09:35 +0100

    Aggiornamento a .NET 5

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace AspNetCoreRealtimeBinary
 {
@@ -30,13 +31,11 @@ namespace AspNetCoreRealtimeBinary
             //Per la demo: registrare SignalR e l'hosted service
             services.AddSignalR().AddMessagePackProtocol();
             services.AddHostedService<ImageGenerator>();
-
-
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -51,21 +50,15 @@ namespace AspNetCoreRealtimeBinary
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
-            //Per la demo: registrazione del middleware di SignalR
-            app.UseSignalR((configure) =>
+            app.UseRouting();
+            app.UseEndpoints(routeBuilder => 
             {
-                configure.MapHub<ImageStreamHub>("/image-stream", options => {
-                    //Limite di peso massimo dell'immagine da inviare al client
+                routeBuilder.MapDefaultControllerRoute();
+                // Registro l'hub che permette al client di fare start/stop
+                routeBuilder.MapHub<ImageStreamHub>("/image-stream", options => {
+                    // Limite di peso massimo dell'immagine da inviare al client
                     options.ApplicationMaxBufferSize = 256 * 1024; //256KB
                 });
-            });
-
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
